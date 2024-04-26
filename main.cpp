fa77871602e5facf0320ef5b6377fd2095dd5cf5
Khumayun <k.aminaddinov@gmail.com>
2024-04-26 16:49:20 +0500

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,27 @@
+//
+// Created by ...
+// Date:
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+    cout << "PROBLEM 1\n";
+    // write your code for problem 1 here...
+
+    cout << "PROBLEM 2\n";
+    // write your code for problem 2 here...
+
+    cout << "PROBLEM 3\n";
+    // write your code for problem 3 here...
+
+    cout << "PROBLEM 4\n";
+    // write your code for problem 4 here...
+
+    cout << "PROBLEM 5\n";
+    // write your code for problem 5 here...
+
+    return 0;
+}
